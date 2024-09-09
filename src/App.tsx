fb168b468e13050e51269d2c34472a6abdcbb3b4
joaquinlari <larijoaquin@gmail.com>
2024-09-09 03:00:10 -0300

    Fix: type function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 const interMiami: number = 11;
 const bocaJuniors: number = 11;
-function App(interMiami: number, bocaJuniors: number): string {
+const App: React.FC<{ interMiami: number; bocaJuniors: number }> = ({
+  interMiami,
+  bocaJuniors,
+}) => {
   if (interMiami > bocaJuniors) return <h2>Gano boca</h2>;
   return <h1>Aprendiendo TypeScript</h1>;
-}
+};
 
 export default App;
