4742e4c532125b8dd674e1c7c8ba696da76c3f4d
joaquinlari <larijoaquin@gmail.com>
2024-09-08 23:53:07 -0300

    learn type

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-function App() {
+const interMiami: number = 11;
+const bocaJuniors: number = 11;
+function App(interMiami: number, bocaJuniors: number): string {
+  if (interMiami > bocaJuniors) return <h2>Gano boca</h2>;
   return <h1>Aprendiendo TypeScript</h1>;
 }
 
