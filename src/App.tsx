30a341b76fb83cff5276b719fa63e35147aa86e1
joaquinlari <larijoaquin@gmail.com>
2024-09-09 03:10:24 -0300

    Fixing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 const interMiami: number = 11;
 const bocaJuniors: number = 11;
-const App: React.FC<{ interMiami: number; bocaJuniors: number }> = ({
-  interMiami,
-  bocaJuniors,
-}) => {
-  if (interMiami > bocaJuniors) return <h2>Gano boca</h2>;
-  return <h1>Aprendiendo TypeScript</h1>;
+const lionelMessi: boolean = true;
+
+const App: React.FC<{
+  interMiami: number;
+  bocaJuniors: number;
+  lionelMessi: boolean;
+}> = ({ interMiami, bocaJuniors, lionelMessi }) => {
+  if (lionelMessi ? (interMiami += 1) : interMiami)
+    return (
+      <>
+        <h1>Aprendiendo TypeScript</h1>
+        <div>
+          {interMiami > bocaJuniors ? (
+            <h2>Ganó Inter de Miami</h2>
+          ) : interMiami === bocaJuniors ? (
+            <h2>Empate</h2>
+          ) : (
+            <h2>Ganó Boca Juniors</h2>
+          )}
+        </div>
+      </>
+    );
 };
 
 export default App;
