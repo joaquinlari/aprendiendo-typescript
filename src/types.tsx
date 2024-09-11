876e5153703dbca46a1a43c6c823b6867b66e721
joaquinlari <larijoaquin@gmail.com>
2024-09-10 22:15:52 -0300

    Understanding array's types

--- src/types.tsx
+++ src/types.tsx
@@ -9,3 +9,12 @@ console.log(variable); // 123, identifica como number
 
 variable = true;
 console.log(variable); // "true", identifica como boolean
+
+//Arreglos
+const arreglo1 = [1, 2, 3, 4, 5];
+const arreglo2 = ["HTML", "CSS", "JAVASCRIPT"];
+
+// arreglo1.push("REACT"); esto no funcionaría ya que ts detecta arreglo1 como tipo number
+
+const arreglo3: number[] = [1, 2, 3, 4, 5];
+const arreglo4: string[] = ["HTML", "CSS", "JAVASCRIPT"];
