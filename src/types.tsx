e3ff72d0265ae552e116590cbccc444123511dc0
joaquinlari <larijoaquin@gmail.com>
2024-09-10 22:01:03 -0300

    Learn ":any"

---
+++ src/types.tsx
@@ -0,0 +1,11 @@
+//El tipo any no es buena practica ya que puede adquirir cualquier valor
+let variable;
+
+variable = "Hola mundo";
+console.log(variable);
+
+variable = 123;
+console.log(variable);
+
+variable = true;
+console.log(variable);
