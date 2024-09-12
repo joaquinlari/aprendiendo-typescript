3716a71ee584d82bec8628f935a0ae5c6a7d453f
joaquinlari <larijoaquin@gmail.com>
2024-09-12 00:02:51 -0300

    learn types

--- src/types.tsx
+++ src/types.tsx
@@ -43,7 +43,11 @@ console.log(programador);
   tecnologias: ["C#"],
   tomaMate: "SI",  esta linea estaria mal ya que al principio 'tomaMate' se definió como booleano solo.
 }; */
-
+type Programador2 = {
+  nombre: string;
+  tecnologias: string[];
+  tomaMate: string;
+};
 const programador2: Programador2 = {
   nombre: "Lari Joaquin",
   tecnologias: ["C#"],
