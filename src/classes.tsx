fbc654f83b71e08a088c1fdde026c65eeffbad67
joaquinlari <larijoaquin@gmail.com>
2024-09-13 06:08:56 -0300

    Learn Constructor

--- src/classes.tsx
+++ src/classes.tsx
@@ -6,6 +6,12 @@ class Peliculas {
   proyectarEnCine() {
     console.log(`La pelicula es ${this.nombre}`);
   }
+
+  constructor(nombre: string, protagonistas: string[], actores: string[]) {
+    this.nombre = nombre;
+    this.protagonistas = protagonistas;
+    this.actores = actores;
+  }
 }
 
 const pelicula = new Peliculas();
