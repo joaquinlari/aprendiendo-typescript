4816325c49813812bf03a2f127d9485b794aee3a
joaquinlari <larijoaquin@gmail.com>
2024-09-15 21:51:28 -0300

    explain '<T>'

--- src/sorteo.tsx
+++ src/sorteo.tsx
@@ -1,3 +1,4 @@
+//la '<T>' todavia no sabemos que es un generico que sirve para pasar un tipo de dato
 class Sorteo<T> {
   private ticket?: T; // private es que no se puede acceder desde afuera, solo puede manejarse en la clase
 
