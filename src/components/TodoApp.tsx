2afdf1ad77ba6248527a8a105016d5176eb5c0e5
joaquinlari <larijoaquin@gmail.com>
2024-09-17 21:16:17 -0300

    skectch input

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,3 +1,13 @@
 export const TodoApp = () => {
-  return <div>TodoApp</div>;
+  return (
+    <div>
+      <h1>Lista de Tareas</h1>
+      <input
+        type="text"
+        value={}
+        onChange={}
+        placeholder="Escriba una nueva tarea"
+      />
+    </div>
+  );
 };
