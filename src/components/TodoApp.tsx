37ce4a1b483ed3a806672bb063a7860301f2bda8
joaquinlari <larijoaquin@gmail.com>
2024-09-17 21:33:21 -0300

    newTarea state

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
+
 export const TodoApp = () => {
+  const [newTarea, setNewTarea] = useState("");
   return (
     <div>
       <h1>Lista de Tareas</h1>
       <input
         type="text"
-        value={}
-        onChange={}
+        value={newTarea}
+        onChange={(e) => setNewTarea(e.target.value)}
         placeholder="Escriba una nueva tarea"
       />
     </div>
