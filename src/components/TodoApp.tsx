6890f733db428430d90d62e1c443951a082555cd
joaquinlari <larijoaquin@gmail.com>
2024-09-19 23:28:00 -0300

    can add tasks

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,28 +1,28 @@
 import { useState } from "react";
-import {ListaTareas} from './ListaTareas'
+import { ListaTareas } from "./ListaTareas";
 
 export const TodoApp = () => {
-  const [newTarea, setNewTarea] = useState<string>("");
-  const [listaTareas, setListaTareas] = useState<string[]>([])
+  const [nuevaTarea, setNuevaTarea] = useState<string>("");
+  const [listaTareas, setListaTareas] = useState<string[]>([]);
 
-  const handleAddTask = () = {
-    
-  }
+  const handleAddTask = () => {
+    if (nuevaTarea.trim() === "") return;
+    setListaTareas((tareasAnteriores) => [...tareasAnteriores, nuevaTarea]);
+    setNuevaTarea("");
+  };
 
-  const handleBorrarTarea = (index:number) => {
-
-  }
+  const handleBorrarTarea = (index: number) => {};
   return (
     <div>
       <h1>Lista de Tareas</h1>
       <input
         type="text"
-        value={newTarea}
-        onChange={(e) => setNewTarea(e.target.value)}
+        value={nuevaTarea}
+        onChange={(e) => setNuevaTarea(e.target.value)}
         placeholder="Escriba una nueva tarea"
       />
       <button onClick={handleAddTask}></button>
-      <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea}/>
+      <ListaTareas listaTareas={listaTareas} borrarTarea={handleBorrarTarea} />
     </div>
   );
 };
